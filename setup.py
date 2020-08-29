962dfc6a4dadce5d8bea21eaee69c4e6e287880c
Chenxiong Qi <qcxhome@gmail.com>
2020-08-29 14:31:27 +0800

    Add description to package
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- setup.py
+++ setup.py
@@ -7,4 +7,6 @@ setup(
 	author_email='qcxhome@gmail.com',
 	license='mit',
 	url='https://github.com/tkdchen/learning-travis-ci',
+	description='An empty package',
+	long_description='An empty package for learning deployment within Travis-CI',
 )
