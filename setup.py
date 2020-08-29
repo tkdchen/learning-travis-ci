bf787acd6598af959814ee19ba31cd7d3ab2ff48
Chenxiong Qi <qcxhome@gmail.com>
2020-08-29 13:16:40 +0800

    initial package
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name='learning-travis-ci',
+	version='0.1',
+	author='Chenxiong Qi',
+	author_email='qcxhome@gmail.com',
+	license='mit',
+	url='https://github.com/tkdchen/learning-travis-ci',
+)
